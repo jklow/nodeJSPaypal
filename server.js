3507c6463eaae815746a14a544790ab8ffb78722
unknown <jklow@sp.edu.sg>
2019-03-29 09:36:16 +0800

    Paypal POC-with Openshift v2

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var serveStatic = require('serve-static');
 var transact=require('./paypal_lib/transact')
 
 var app = express();
-var port=3000;
-var hostname="localhost";
+var port=process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var hostname=process.env.OPENSHIFT_NODEJS_IP || "localhost";
 
 app.use(bodyParser.json());
 
@@ -17,7 +17,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(urlencodedParser);//attach body-parser middleware
 
 
-console.log(process.env.PAYPAL_CLIENT_ID);
+//console.log(process.env.PAYPAL_CLIENT_ID);
 app.post('/api/create-paypal-transaction', function (req, res) {
   console.log("API create paypal transaction called!");
   console.log(req.body);  
